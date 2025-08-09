2ef25f7dc3e7b01d84c93c6a0a336c541d174d84
TrueBuch <theduak.dev@gmail.com>
2025-08-09 21:09:01 +0500

    sdafsdf

--- Assets/Scripts/Radio.cs
+++ Assets/Scripts/Radio.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Radio : MonoBehaviour
 {
@@ -17,7 +19,7 @@ public class Radio : MonoBehaviour
 
     private void Start()
     {
-        _audioSource.clip = Content.AudioNoise;
+        _audioSource.clip = Content.AudioZnaesh;
         _audioSource.volume = 0;
         _audioSource.Play();
         StartCoroutine(AAASFFFF());
@@ -28,17 +30,43 @@ public class Radio : MonoBehaviour
         var volume = _volume.CurrentValue;
         var tuning = _tuning.CurrentValue;
 
-        _audioSource.volume = volume;
+        //_audioSource.volume = volume;
     }
 
     private IEnumerator AAASFFFF()
     {
         while (true)
         {
-            _audioSource.Pause();
+            _audioSource.volume = 0f;
             yield return new WaitForSeconds(Random.Range(0.03f, 0.05f));
-            _audioSource.UnPause();
+            _audioSource.volume = _volume.CurrentValue;
             yield return new WaitForSeconds(Random.Range(0.03f, 0.05f));
         }
     }
+}
+
+public class TagWave : Tag
+{
+    [SerializeReference] SerializedDictionary<AudioClip, FloatRange> ClipsDON = new();
+}
+public class Waves
+{
+
+    // SW : 3MHz - 30MHz 
+    // AM : 540KHz - 1600KHz 
+    // FM : 88MHz - 108 MHz
+
+    //ДИАПАЗОНЫ - АУДИОФАЙЛЫ
+    //ПОМЕХИ
+}
+
+[System.Serializable]
+public struct FloatRange
+{
+    
+    [MinMaxSlider(3f, 30f, ShowFields = true)]
+    public Vector2Int Range;
+
+    public int Min => Range.x;
+    public int Max => Range.y;
 }
